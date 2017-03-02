1e57f303e790055b5695364b302dffea78e73871
aluno <aluno>
2017-03-02 16:18:19 -0300

    aula Dados Primitivos

---
+++ 01 - Tipos Primitivos/TiposPrimitivos/TiposPrimitivos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposPrimitivos
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int numero = 3;
+            float numero2 = 2f;
+            bool certidaoDeVivo = false;
+            int resultado = numero + (int) numero2;
+            string texto = "Ola mundo";
+
+            enum Dias  = {
+            domingo,segunda,terça,quarta,quinta,sexta,sabado
+             };
+
+            
+
+
+            Console.WriteLine(numero);
+            Console.WriteLine(texto);
+
+            Console.WriteLine(texto+"Voce esta vivo {0}", certidaoDeVivo);
+
+            Console.ReadLine();
+
+            // Comentário em Chsarp é escrito com // {tambem}
+
+
+        }
+    }
+}
